c335cef722a12c1dd59e203f8a2234a536ff54ec
renatons17 <83104807+renatons17@users.noreply.github.com>
2021-08-03 23:48:06 -0300

    Implementando PUT no componente postagem-edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
+import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
 import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
 import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 import { TemaComponent } from './tema/tema.component';
@@ -6,8 +8,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { InicioComponent } from './inicio/inicio.component';
-import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
-import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
+
 
 const routes: Routes = [
 
@@ -20,8 +21,10 @@ const routes: Routes = [
 
   {path: 'tema-edit/:id', component: TemaEditComponent},
   {path: 'tema-delete/:id', component: TemaDeleteComponent},
-  {path: 'postagem-edit/:id', component: PostagemEditComponent}, 
+  {path: 'postagem-edit/:id', component: PostagemEditComponent},
   {path: 'postagem-delete/:id', component: PostagemDeleteComponent}
+
+
 
 ];
 
